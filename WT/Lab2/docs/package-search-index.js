5526ee5a897a8a268af8ba1002bd79fb4e3df59e
dungeon-slave <70040363+dungeon-slave@users.noreply.github.com>
2023-02-08 14:29:38 +0300

    Delete WT/Lab2 directory

--- WT/Lab2/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"data.xml_serialization"},{"l":"data.xml_serialization.model"},{"l":"logic.entity"},{"l":"screen"}];updateSearchResults();
